ae3e70127d3e5335f84c9b2b8afd28ee38e23b99
JuliaHsiao <juliaxiao860508@gmail.com>
2021-12-15 14:27:08 +0800

    2021/12/15 14:27

--- dice/ViewController.swift
+++ dice/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var SumLabel: UILabel!
+    @IBOutlet weak var DiceLabel: UILabel!
+    @IBOutlet weak var DiceStepper: UIStepper!
+    
+    var numOfDice:Int = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        DiceStepper.value = 1
+        DiceLabel.text = Int(DiceStepper.value).description
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func getNumberOfDices(_ sender: UIStepper) {
+        DiceLabel.text = Int(sender.value).description
+        
+        SumLabel.isHidden = true
+    }
+    
+    
+    @IBAction func roll(_ sender: UIButton) {
+        numOfDice = Int(DiceStepper.value)
+        var sum = 0
+        
+        // genterate the random int
+        for i in 1...numOfDice{
+            let diceValue = Int.random(in: 1...6)
+            print(diceValue)
+            sum += diceValue
+            let diceIndex = i
+        }
+        print(sum)
+        SumLabel.text = "Sum:\n\(sum)"
+        SumLabel.isHidden = false
+    }
 }
 
